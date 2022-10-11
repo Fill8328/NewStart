854e8b69131c455f35150789cb4dc4c4ba8e94f9
Fill8328 <84182043+Fill8328@users.noreply.github.com>
2022-10-11 22:05:00 +0300

    commit

---
+++ src/fill/jma/Cube.java
@@ -0,0 +1,23 @@
+package fill.jma;
+
+public class Cube {
+    private int k;
+
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    public int getK() {
+        return k;
+    }
+
+    public int cube() {
+        {
+            int y;
+            y = k * k * k;
+            return y;
+
+        }
+
+    }
+}
---
+++ src/fill/jma/Main.java
@@ -0,0 +1,14 @@
+package fill.jma;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cube Math =new Cube();
+        Math.setK(4);
+        System.out.println("куб числа "+ Math.getK() + " равен "+ "\n" +Math.cube());
+
+    }
+
+}
